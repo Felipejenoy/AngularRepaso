74c6e798bce957451846ba2545d723f81c12f370
Felipe Jenoy <johanordonez@unicauca.edu.co>
2025-09-13 18:23:27 -0500

    Se acomodó la destructuración de la clase 06

--- 01-typescript-intro/src/topics/06-function-destructuring.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -16,10 +16,11 @@ interface TaxCalculationOptions{
     tax :number,
     products: Product[];
 }
+
 function taxCalculation(options:TaxCalculationOptions):number[]{
     let total =0;
-    options.products.forEach(product =>{
-        total += product.price;
+    options.products.forEach(({price}) =>{
+        total +=price;
     })
     return [total, total * options.tax]
 }
@@ -29,12 +30,12 @@ const shopinCart = [phone, tablet];
 const tax =0.15;
 
 
-const result = taxCalculation({
+const [total, taxTotal] = taxCalculation({
     products :shopinCart,
     tax:tax
 });
 
-console.log('total', result[0]);
-console.log('tax', result[1]);
+console.log('total',total);
+console.log('tax', taxTotal);
 console.log()
 export{};  
